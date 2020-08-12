8e6dfe344cfb614217328799d44b57cd1bf8dd1f
catiumeye <catiumeye@gmail.com>
2020-08-13 01:00:07 +0500

    Slider demo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-let p = document.querySelectorAll('p');
+let img = document.querySelector('.slider-img');
+let lb = document.querySelector('#left');
+let rb = document.querySelector('#right');
 
-for (i = 0; i < p.length; i++){
-    p[i].style.background = 'red';
+let sl = 0;
+
+lb.addEventListener('click', levo);
+
+function levo(){
+    if(sl < -3200 || sl > -400 ){
+        sl = 0;
+    }
+    sl += 400;
+    img.style.left = sl + 'px';
 }
 
+rb.addEventListener('click', pravo);
+
+function pravo(){
+    if(sl < -3200 || sl > 0 ){
+        sl = 0;
+    }
+    sl -= 400;
+    img.style.left = sl + 'px'; 
+}
